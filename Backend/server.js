ba43c03895ad6b003887152a7690f36419d91e04
thenishantnayan <nayannishant999@gmail.com>
2026-07-04 04:02:01 +0530

    Use process.env.PORT for deployment

--- Backend/server.js
+++ Backend/server.js
@@ -2,11 +2,16 @@ require("dotenv").config();
 
 const app = require("./src/app")
 
-app.listen(3000,()=>{
+const PORT = process.env.PORT || 3000;
 
-   console.log("server is running on port 3000");
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+// app.listen(3000,()=>{
 
-})
+//    console.log("server is running on port 3000");
+
+// })
 
   
 
