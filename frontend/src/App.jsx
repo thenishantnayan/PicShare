bf84e2ab621bfa056ccda8381b7b9c8aa37fbd2f
thenishantnayan <nayannishant999@gmail.com>
2026-07-04 05:45:03 +0530

    app.jsx changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import CreatePost from "./pages/createpost";
 import Feed from "./pages/feed";
 
@@ -6,7 +6,10 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/feed" element={< Feed/>} />
+        {/* Redirect / to /create-post */}
+        <Route path="/" element={<Navigate to="/create-post" replace />} />
+
+        <Route path="/feed" element={<Feed />} />
         <Route path="/create-post" element={<CreatePost />} />
       </Routes>
     </Router>
